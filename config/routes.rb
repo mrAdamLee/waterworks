87c00460b5553b120866904d0ffd1de5d9ea8f16
mrAdamLee <adamangel@southern.edu>
2023-11-12 12:37:24 +0100

    added utility billing promo page, and route to get there along with controller method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  get 'home/utility_billing', to: 'home#utility_billing'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
