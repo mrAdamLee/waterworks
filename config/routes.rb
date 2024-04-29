2113797d77729d36188d6eda2111b2cb5d66d257
mrAdamLee <adamangel@southern.edu>
2024-04-29 13:04:30 -0400

    sessions/new route added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/utility_billing', to: 'home#utility_billing'
+  get 'sessions/new' 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
