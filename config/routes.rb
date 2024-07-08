ca9ea2105f68aba3e89a823641b0cb3eab49b73a
mrAdamLee <adamangel@southern.edu>
2024-07-08 18:04:52 -0400

    route to subdomain landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  constraints(SubdomainConstraint) do 
+    root to: 'landing_pages#show', as: :subdomain_root 
+  end 
   root 'home#index'
   get 'home/utility_billing', to: 'home#utility_billing'
   get 'sessions/new' 
